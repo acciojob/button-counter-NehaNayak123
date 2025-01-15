d5994789e550e66b40abda24081c78e2d6f79252
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-01-15 18:21:26 +0530

    Create Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export const Button =()=>{
+    const [count, setCount]=useState(0)
+    return(
+        <div>
+            <p>Button clicked {count} times</p>
+            <button onClick={()=>setCount(count+1)}>Click me</button>
+        </div>
+    )
+}
